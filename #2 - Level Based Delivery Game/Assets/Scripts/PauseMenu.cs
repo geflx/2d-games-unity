ad61272b71b90237df21b599f14fe33e33653ed1
Gabriel Felix <gabrielfelix2500@gmail.com>
2020-05-22 01:56:34 -0300

    Pause Menu added and change on menu background

---
+++ #2 - Level Based Delivery Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(gameIsPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; //This variable can be used to do slow motion
+        gameIsPaused = false;
+    }
+
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame(){
+        Debug.Log("Quiting game...");
+        Application.Quit();
+    }
+}
