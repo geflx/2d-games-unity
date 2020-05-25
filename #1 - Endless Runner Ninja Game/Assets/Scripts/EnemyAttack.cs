0ef197826e0ac417a85d59953e17df5e1a6eeced
Gabriel Felix <gabrielfelix2500@gmail.com>
2020-05-25 02:23:07 -0300

    Starting to work on Menu and adding Enemy Character

---
+++ #1 - Endless Runner Ninja Game/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+  
+    public float delayTime;
+    public int speed;
+
+    void Update()
+    {
+       delayTime -= Time.deltaTime;
+       
+       if(delayTime <= 0.0f){
+           transform.Translate(Vector2.left * speed * Time.deltaTime);
+       } 
+    }
+}
--- #1 - Endless Runner Ninja Game/Assets/Scripts/Spawner.cs
+++ #1 - Endless Runner Ninja Game/Assets/Scripts/Spawner.cs
@@ -17,7 +17,9 @@ public class Spawner : MonoBehaviour
     public int minIndexKamaboko;
     public int maxIndexKamaboko;
 
-
+    public int minIndexShuriken;
+    public int maxIndexShuriken;
+    
     private void Update(){
         if(timeBtwSpawn <= 0){
 
